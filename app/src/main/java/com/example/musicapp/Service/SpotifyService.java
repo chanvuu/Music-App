241daa8c1f3c5cad1db584f1de0f77eaa8f779ee
chanvuu <caochanvu2002@gmail.com>
2024-04-08 11:03:08 +0700

    update more service

--- app/src/main/java/com/example/musicapp/Service/SpotifyService.java
+++ app/src/main/java/com/example/musicapp/Service/SpotifyService.java
@@ -1,8 +1,16 @@
 package com.example.musicapp.Service;
 
+import com.example.musicapp.Model.FeaturedPlaylists;
+import com.example.musicapp.Model.PlaylistBase;
+import com.example.musicapp.Model.PlaylistSimple;
 import com.example.musicapp.Model.TokenResponse;
 
 import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.AlbumsPager;
+import kaaes.spotify.webapi.android.models.Playlist;
+import kaaes.spotify.webapi.android.models.PlaylistsPager;
+import kaaes.spotify.webapi.android.models.UserPrivate;
+import kaaes.spotify.webapi.android.models.UserPublic;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -10,6 +18,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface SpotifyService {
     @FormUrlEncoded
@@ -21,8 +30,20 @@ public interface SpotifyService {
             @Field("client_id") String clientId,
             @Field("client_secret") String clientSecret
     );
-
     @GET("albums/{id}")
     Call<Album> getAlbum(@Path("id") String id, @Header("Authorization") String accessToken);
+    @GET("albums")
+    Call<Album> getAlbum(@Header("Authorization") String accessToken);
+    @GET("me")
+    Call<UserPublic> getCurrentUser(@Header("Authorization") String accessToken);
+    @GET("browse/new-releases")
+    Call<AlbumsPager> getNewReleases(@Header("Authorization") String accessToken, @Query("limit") int limit);
+    @GET("browse/featured-playlists")
+    Call<FeaturedPlaylists> getFeaturedPlaylists(@Header("Authorization") String accessToken, @Query("limit") int limit, @Query("offset") int offset, @Query("locale") String locale);
+    @GET("playlists/{playlist_id}")
+    Call<Playlist> getPlayList(@Path("playlist_id") String playlist_id, @Header("Authorization") String accessToken);
+//    @GET("search")
+//    Call<PlaylistSimple> search(@Header("Authorization") String accessToken, @Query("q") String q, @Query("type") String type);
+
 }
 
